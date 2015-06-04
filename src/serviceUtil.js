64dab87caa7e00ca461b188a3afd00a71deb3cb4
Miguel Jimenez <mjimenez@rocketlawyer.com>
2015-06-04 13:39:17 -0700

    removing localhost check

--- src/serviceUtil.js
+++ src/serviceUtil.js
@@ -1,14 +1,8 @@
 rlUtil.factory('serviceUtil', ['$location', '$q', 
   function($location, $q) {
-    var host = $location.host();
-    if (host === 'localhost') {
-      host = 'localhost:9292/cms5.dev.rocketlawyer.com';
-      // for local endpoints
-      // host = "localhost:9292/localhost:8080/";
-    }
     return {
-      serviceBase: $location.protocol() + '://' + host + '/catalog-admin/',
-      taxonomyBase: $location.protocol() + '://' + host + '/lighthouse/cmstaxonomy/v1/',
+      serviceBase: '/catalog-admin/',
+      taxonomyBase: '/lighthouse/cmstaxonomy/v1/',
       taxonomyUUID: '1d150cc9-7749-40ea-82d5-3b1ed3fc5981',
       taxonomyName: 'Products',
       successFilter: function(result) {
